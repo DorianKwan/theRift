d0741f544ba1935b1e0b526011d9425187dd92c4
Bryce Sayers-Kwan <bryce.sayers-kwan@live.ca>
2019-03-10 20:52:44 -0600

    Added connected to server

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,19 @@ import "./app.scss"
 import MatchHistory from "./pages/MatchHistory";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.connectServer = this.connectServer.bind(this);
+  }
+
+  connectServer() {
+    fetch("/");
+  }
+
+  componentDidMount() {
+    this.connectServer();
+  }
+
   render() {
     return (
     <div>
